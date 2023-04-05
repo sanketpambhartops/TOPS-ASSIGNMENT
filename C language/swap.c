12ef94a3f778b0fc236cc1e8218498b9ecefd315
sanketpambhartops <128216370+sanketpambhartops@users.noreply.github.com>
2023-04-05 10:00:09 +0530

    assignment
    assignment

---
+++ C language/swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter value of a=");
+    scanf("%d",&b);
+    printf("Enter value of b=");
+    scanf("%d",&a);
+    printf("value a=%d\n",a);
+    printf("value b=%d\n",b);
+    return 0;
+}
