c36d5cccfa53429f4508d8ffb8e7b96145f10f57
sanketpambhartops <128216370+sanketpambhartops@users.noreply.github.com>
2024-01-21 14:22:58 +0530

    done
    done

--- Prectice Project/src/models/user.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-let userSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    number: Number,
-});
-
-export default mongoose.model("user", userSchema);
